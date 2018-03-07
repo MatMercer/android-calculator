790677ec0ddf575d81844d5a38697054aadc10d2
MatMercer <mateusmercer@gmail.com>
2018-03-07 13:23:03 -0300

    Initial commit

---
+++ app/src/main/java/net/matmercer/android/calculator/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package net.matmercer.android.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
